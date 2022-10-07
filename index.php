4fbf912c5594ec33f9d187b74e4888ec5d998de5
David Zapata <jdavid.zapatab@gmail.com>
2022-10-07 16:26:19 +0000

    Updating index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
 // Sample PHP file to test the built-in php server.
-// docker run --rm -it -p 80:80 -v $(pwd):/var/www davidzapata/php-composer-alpine php -S 0.0.0.0:80
+// docker run --rm -it -p 80:80 -v $(pwd):/var/www davidzapata/php-composer-alpine:8.1 php index.php
 phpinfo();
